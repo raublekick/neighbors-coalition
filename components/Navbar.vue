c2c5bc9a38ef03ebaa74d8cd9adc736b9c621017
Andrew Raub <raublekick@gmail.com>
2021-11-26 13:56:02 -0700

    Basic home page and layout

---
+++ components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-navbar class="has-shadow is-spaced" wrapper-class="container">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ name: 'index' }">
+        FP
+      </b-navbar-item>
+    </template>
+    <template slot="start"> </template>
+
+    <template slot="end"
+      ><b-navbar-item
+        tag="router-link"
+        class="has-text-weight-bold"
+        :to="{ name: 'index' }"
+      >
+        Home
+      </b-navbar-item>
+      <!-- <b-navbar-item
+        tag="router-link"
+        class="has-text-weight-bold"
+        :to="search"
+      >
+        Search
+      </b-navbar-item>
+      <b-navbar-item
+        tag="router-link"
+        class="has-text-weight-bold"
+        :to="{ name: 'faqs' }"
+      >
+        FAQs
+      </b-navbar-item>
+      <b-navbar-item
+        tag="router-link"
+        class="has-text-weight-bold"
+        :to="{ name: 'about' }"
+      >
+        About
+      </b-navbar-item> -->
+    </template>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,36 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+      <div class="column has-text-centered feature mr-4">
+        <nuxt-link :to="{ name: 'community' }">
+          <div>
+            <b-icon icon="hand-heart" size="is-large" title="Help me" />
+            <p class="title is-4">Help me</p>
+            <p class="subtitle">
+              I need assistance finding resources.
+            </p>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="column has-text-centered feature">
+        <nuxt-link :to="{ name: 'community' }">
+          <div>
+            <b-icon icon="handshake" size="is-large" title="I want to help" />
+            <p class="title is-4">I want to help</p>
+            <p class="subtitle">
+              Find opportunities to help your neighbors.
+            </p>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'HomePage',
 
-  components: {
-    Card
-  }
+  components: {}
 }
 </script>
