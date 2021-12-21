e7f856cede10edb20687255e913afd7d6538cd21
Andrew Raub <raublekick@gmail.com>
2021-12-21 16:25:12 -0700

    Adding give-help

---
+++ pages/give-help.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <b-icon icon="hand-heart" class="is-pulled-left mr-4" size="is-large" title="Help me" />
+    <p class="title is-4">Help me</p>
+    <p class="subtitle">
+      I need assistance finding resources.
+    </p>
+    <div class="columns is-multiline">
+        <feature class="column is-half" route="/search/housing" icon="home" title="Shelter" sub-title="I want to help with shelter." />
+        <feature class="column is-half" route="/search/substance" icon="needle" title="Substance abuse programs" sub-title="I am interested in substance abuse programs." />
+        <feature class="column is-half" route="/search/resource-exchange" icon="currency-usd-off" title="Resource exchanges" sub-title="Help me find resource exchange events." />
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {return {}}
+}
+</script>
