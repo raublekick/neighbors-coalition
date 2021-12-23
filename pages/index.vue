349a227b18113ccc4b7310c82f59c6fbe54e9470
Andrew Raub <raublekick@gmail.com>
2021-12-23 16:02:45 -0700

    Fixing index page with new buttons

--- pages/index.vue
+++ pages/index.vue
@@ -7,18 +7,13 @@
           </div>
         </section>
     <div class="columns is-multiline">
-      <feature class="column is-half" route="help-me" icon="hand-heart" title="Help me" sub-title="I need assistance finding resources." />
-      <feature class="column is-half" route="give-help" icon="handshake" title="I want to help" sub-title="Find opportunities to help your neighbors." />
+      <feature class="column is-one-third" route="help-me" icon="hand-heart" title="Help me" sub-title="I need assistance finding resources." />
+      <feature class="column is-one-third" route="give-help" icon="handshake" title="I want to help" sub-title="Find opportunities to help your neighbors." />
+      <feature class="column is-one-third" route="calendar" icon="calendar" title="Event calendar" sub-title="Weekly events throughout the valley." />
     </div>
 
     <div class="has-text-centered">
       <nuxt-link :to="{ name: 'all' }">View all</nuxt-link>
-    </div>
-
-    <div>
-      <div v-for="(event,index) in events" :key="index" class="pb-3">
-        <event :event="event"/>
-      </div>
     </div>
   </section>
 </template>
