283bd68ffa4cbbfb03fdb6e38050bcd6a6335a98
Andrew Raub <raublekick@gmail.com>
2022-10-23 14:47:49 -0700

    Updating script for geolocation

--- show-cleanup/cleanup.py
+++ show-cleanup/cleanup.py
@@ -1,10 +1,14 @@
 import csv
+import requests
+import re
 line = 0
 last_blank_line = -1
 current_header = ""
 current_category = ""
 current_resource = ""
-new_fields = [ 'Category', 'Name', 'Tags', 'Services', 'Eligibility', 'LatLng', 'Lat',	'Lng', 'Address', 'Hours of Operation',	'Phone', 'Website',	'Email', 'Social Media', 'Rating', 'Facebook', 'Instagram', 'Twitter' ]
+api_url_base = 'https://nominatim.openstreetmap.org/search?format=json&q='
+new_fields = ['Category', 'Name', 'Tags', 'Services', 'Eligibility', 'LatLng', 'Lat',	'Lng', 'Address',
+              'Hours of Operation',	'Phone', 'Website',	'Email', 'Social Media', 'Rating', 'Facebook', 'Instagram', 'Twitter', 'Source']
 new_rows = []
 with open('show.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -17,9 +21,11 @@ with open('show.csv', newline='') as csvfile:
 
         # header rows
         elif (row['Resource Name'] != '' and row['Contact Phone'] == '' and row['Website Address'] == '' and row['Location (if applicable)'] == '' and row['NOTES'] == ''):
-            #primary category
+            # primary category
             if (line == last_blank_line + 1):
                 current_header = row['Resource Name']
+                # reset category
+                current_category = ''
                 print('Line ' + str(line) +
                       ' is a header: ' + current_header)
             if (line != last_blank_line + 1):
@@ -31,14 +37,27 @@ with open('show.csv', newline='') as csvfile:
         else:
             # resources with on multiple lines will only have the resource on the first row of that 'block'
             # store the resource name if it's found and use it for subsequent blank resource rows
-            if(row['Resource Name'] != ''):
+            if (row['Resource Name'] != ''):
                 current_resource = row['Resource Name']
             else:
                 resource = current_resource
                 row['Resource Name'] = resource
-            print('Line ' + str(line) + ':' + str(row))
-            new_rows.append([current_header, current_resource, '', row['NOTES'], current_category, '', '', '', row['Location (if applicable)'], '', row['Contact Phone'], row['Website Address'], '', '', '', '', '', ''])
-        
+            #print('Line ' + str(line) + ':' + str(row))
+
+            # try to get lat/lng
+            latlng = ''
+            if (row['Location (if applicable)'] != ''):
+                response = requests.get(
+                    api_url_base + row['Location (if applicable)'])
+                result = response.json()
+                if result:
+                    latlng = str(result[0]['lat']) + \
+                        ', ' + str(result[0]['lon'])
+            new_row = [current_header, current_resource, '', row['NOTES'], current_category, latlng, '', '',
+                       row['Location (if applicable)'], '', row['Contact Phone'], row['Website Address'], '', '', '', '', '', '', 'SHOW Clinic']
+            new_rows.append(new_row)
+            print(str(new_row))
+
         # if (line <= 16):
         #     print('Line ' + str(line) + ' ' + str(row))
 
@@ -47,14 +66,14 @@ with open('show.csv', newline='') as csvfile:
 
 # create new csv
 filename = "show-cleaned.csv"
-    
-# writing to csv file 
-with open(filename, 'w') as csvfile: 
-    # creating a csv writer object 
-    csvwriter = csv.writer(csvfile) 
-        
-    # writing the fields 
-    csvwriter.writerow(new_fields) 
-        
-    # writing the data rows 
+
+# writing to csv file
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
+
+    # writing the fields
+    csvwriter.writerow(new_fields)
+
+    # writing the data rows
     csvwriter.writerows(new_rows)
